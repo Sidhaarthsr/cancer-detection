7b1feeea2d8696ab1c04c4b2baef44f0b8ba25a7
Shail Shah <shail251298@gmail.com>
2023-06-12 13:36:52 -0400

    added model3 with batchnorm, dropout, residuals

--- model.py
+++ model.py
@@ -44,3 +44,39 @@ class Modelv2(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+    
+class Modelv3(nn.Module):
+    def __init__(self):
+        super(Modelv3, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.dropout = nn.Dropout2d(0.8)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(256*64*64, 128)
+        self.fc2 = nn.Linear(128, 5)
+
+    def forward(self, x):
+        residual = x
+        out = F.relu(self.maxpool(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out += residual
+        out = F.relu(self.dropout(out))
+        out = F.relu(self.bn3(self.conv3(out)))
+        out = F.relu(self.bn4(self.conv4(out)))
+        out += residual
+        out = F.relu(self.dropout(out))
+        out = F.relu(self.bn5(self.conv5(out)))
+        out = self.dropout(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
